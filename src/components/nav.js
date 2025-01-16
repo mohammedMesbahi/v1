34329ba2a5a78b2bf4b88ca35917f1bc034be180
mohammed mesbahi <116631139+mohammedMesbahi@users.noreply.github.com>
2025-01-16 14:27:06 +0100

    the resume link point to an external provider

--- src/components/nav.js
+++ src/components/nav.js
@@ -206,7 +206,11 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+    <a
+      className="resume-button"
+      href="https://cvdesignr.com/p/676021095e901"
+      target="_blank"
+      rel="noopener noreferrer">
       Resume
     </a>
   );
